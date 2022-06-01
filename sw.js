d55eafb534ea85b4f2a11f2c7d92d86d0c301634
miguelapol <apolonio1995@gmail.com>
2022-06-01 13:30:04 -0500

    se Ãalñadio para que redireccione cuando se encuentre una pagina

--- sw.js
+++ sw.js
@@ -4,11 +4,12 @@ const nombreCache='apv-v1';
 const archivos = [
     '/',
     'index.html',
+    'error.html',
     'css/bootstrap.css',
     'css/styles.css',
     'js/app.js',
     'js/apv.js',
-    'manifest.json'
+    'manifest.json',
 ];
 
 //cuando se instala el service worker, se registra en el navegador y se guarda en el cache
@@ -31,13 +32,12 @@ self.addEventListener('activate', e => {
 
 //evento fetch para descargar evcentos estaticos
 self.addEventListener('fetch', e => {
-    console.log('fetch ...',e);
+    console.log('Fetch', e)
+   
     e.respondWith(
-        caches.match(e.request)
-        .then(res => {
-            return res;
-})
-        
-    );
-
-})
+      caches
+        .match(e.request)
+        .then(cacheResponse => (cacheResponse ? cacheResponse : caches.match('error.html')))
+      
+    )
+  })
